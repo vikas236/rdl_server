d830c9d3e4a587dd6a884366681e576900af08be
vikas236 <saivikas460@gmail.com>
2024-02-29 02:24:41 +0530

    add cors access

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const cors = require("cors"); // Import cors
 const app = express();
+
+// Use cors middleware
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
