2c1f172f83f3cd040797b8a89e67917e6a425fb7
vikas236 <saivikas460@gmail.com>
2024-03-04 09:57:45 +0530

    add  database connection postgresql

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const express = require("express");
-const cors = require("cors"); // Import cors
 const app = express();
+const query = require("./query");
 
-// Use cors middleware
-app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+// Middleware to enable CORS
+app.use((req, res, next) => {
+  // Set the Access-Control-Allow-Origin header to allow requests from any origin
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // Set other CORS headers as needed
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  // Call next middleware in the chain
+  next();
 });
 
+// Define routes
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/get_data", (req, res) => {
+  query.e.getTable("menu").then((result) => {
+    res.send(result);
+  });
+});
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+app.post("/send_data", (req, res) => {
+  // Access the JSON data sent from the client
+  const objects = req.body;
+  query.e.updateData(objects);
+  res.json({ message: "Data received successfully" });
+});
+
+// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
