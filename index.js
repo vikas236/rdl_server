e0b1800336c289add95e8143be7c118103295217
vikas236 <saivikas461@gmail.com>
2024-03-05 09:19:26 +0530

    add login credentials

--- index.js
+++ index.js
@@ -34,14 +34,14 @@ app.get("/get_data", async (req, res) => {
   }
 });
 
-// app.get("/get_data", async (req, res) => {
-//   try {
-//     const result = await getTable("menu");
-//     res.send(result);
-//   } catch (error) {
-//     res.status(500).send("Error occurred while checking database.");
-//   }
-// });
+app.get("/get_data", async (req, res) => {
+  try {
+    const result = await getTable("login_credentials");
+    res.send(result);
+  } catch (error) {
+    res.status(500).send("Error occurred while checking database.");
+  }
+});
 
 // Middleware to parse JSON bodies
 app.use(express.json());
