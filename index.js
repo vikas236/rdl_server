c0e769be2a640aea0bf0e930ffeb352f18fd7f3d
vikas236 <saivikas461@gmail.com>
2024-03-05 09:20:52 +0530

    add update

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get("/get_data", async (req, res) => {
   }
 });
 
-app.get("/get_data", async (req, res) => {
+app.get("/get_cred", async (req, res) => {
   try {
     const result = await getTable("login_credentials");
     res.send(result);
